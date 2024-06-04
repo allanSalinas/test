354b8095b433cf4f3b6de43cc97a61689fb653fe
allanSalinas <alla.salinas@duocuc.cl>
2024-06-03 21:56:17 -0400

    Create diccionario.py

---
+++ diccionario.py
@@ -0,0 +1,17 @@
+usuario = {
+    "username" : "allan", 
+    "email" : "alla.salinas@duocuc.cl",
+    "job" : "seguridad",
+    "age" : "27",
+    "phone" : "+56978643512"
+}                               
+print(len(usuario))
+
+
+obras_de_autores = {
+    "autores":{
+        "name" : "jorge",
+        "jobs_names" : ["otoño", "llanura", "espacio"]
+    }
+}
+print(obras_de_autores["autores"]["jobs_names"][0])
