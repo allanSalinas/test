c10cae529c7d68a203e85a48d2397e7e03311f6d
allanSalinas <alla.salinas@duocuc.cl>
2024-06-03 22:14:19 -0400

    Update nombres.py

--- nombres.py
+++ nombres.py
@@ -4,4 +4,4 @@ nombre2 = input("Ingresa el segundo nombre: ")
 nombre3 = input("Ingresa el tercer nombre: ")
 nombres = [nombre1, nombre2, nombre3]
 nombre_mas_largo = max(nombres, key=len)
-print(f"El nombre con la mayor cantidad de caracteres es: {nombre_mas_largo}")
+print("El nombre con la mayor cantidad de caracteres es:",nombre_mas_largo)
