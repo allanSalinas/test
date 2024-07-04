c232d3e85c55c667e635c508f7b327b1cf1e9647
allanSalinas <alla.salinas@duocuc.cl>
2024-07-03 21:46:28 -0400

    se borran archivos

--- nombres.py
+++
@@ -1,7 +0,0 @@
-
-nombre1 = input("Ingresa el primer nombre: ")
-nombre2 = input("Ingresa el segundo nombre: ")
-nombre3 = input("Ingresa el tercer nombre: ")
-nombres = [nombre1, nombre2, nombre3]
-nombre_mas_largo = max(nombres, key=len)
-print("El nombre con la mayor cantidad de caracteres es:",nombre_mas_largo)
